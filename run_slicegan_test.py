bb9120a725b88e541a9e87fea2b3869f194b85bf
Joshua Stickel <jsticke3@jh.edu>
2022-02-10 16:13:11 -0500

    fix run_dream3d_test

--- run_slicegan_test.py
+++ run_slicegan_test.py
@@ -13,7 +13,7 @@ import json
 # Run with False to show an image during or after training
 parser = argparse.ArgumentParser()
 parser.add_argument('training', type=int)
-parser.add_argument("--inp", dest="path_input", default="inputs_2d.json", type=str, help="Specify input file location")
+parser.add_argument("--inp", dest="path_input", default="inputs_3d.json", type=str, help="Specify input file location")
 args       = parser.parse_args()
 Training   = args.training
 path_input = args.path_input
@@ -21,12 +21,15 @@ path_input = args.path_input
 # Import user variables
 with open(path_input, 'r') as f:
     data = json.load(f)["main"]
+    
 ### SliceGAN variables
+
 ## Output structuring
 # Used as folder in output directory and name for output files
 Project_name       = data["Project_name"      ]
 # Output directory
 Project_dir        = data["Project_dir"       ]
+
 ## Data Processing
 # Define image  type (colour, grayscale, three-phase or two-phase.
 # n-phase materials must be segmented)
@@ -38,18 +41,15 @@ data_type          = data["data_type"         ]
 data_path          = data["data_path"         ]
 # Normalize data from color [0,255] to [0,1]
 Normalize          = data["Normalize"         ]
+# Scale factor vs raw data
+scale_factor       = data["scale_factor"      ]
+
 ## Network Architectures
 # should a 2D or 3D microstructure be created
 n_dims             = data["n_dims"            ]
-# Training image size
-# must be a power of 2
-img_size           = data["img_size"          ]
 # Number of image channels (rbg=3, rbga=4, etc)
 img_channels       = data["img_channels"      ]
-# Scale factor vs raw data
-scale_factor       = data["scale_factor"      ]
-# z vector depth
-z_channels         = data["z_channels"        ]
+
 ### DREAM.3D support (preprocessor for ebsd scans, postprocessor for .dream3d files)
 # enable DREAD.3D support? (True/False)
 Dream3d_Support    = data["Dream3d_Support"   ]
@@ -70,10 +70,7 @@ plane_rotations    = data["plane_rotations"   ]
 # EX: tif_names=AA7050, plane_suffixes=["YZ","XY","XY"]
 # will be renamed to "AA7050_YZ.tif", "AA7050_XZ.tif", "AA7050_XY.tif"
 tif_names          = data["tif_names"         ]
-# current valid entries are:
-# "Quats"
-# "EulerAngles"
-# "GBManhattanDistances"
+# valid entries are specified under dream3d_support/properties.json:
 orientations_types = data["orientations_types"]
 # the name of the orientation data in the .dream3d files
 # only applicable if .dream3d files were specified in ebsd_paths
@@ -82,6 +79,7 @@ orientations_names = data["orientations_names"]
 # helps determine if plane_rotations is correct.
 # plane_rotations is correct if histograms overlap
 plot_orientations  = data["plot_orientations" ]
+
 ## Microstructure Generation
 # determines if the direction should be rendered as periodic (True/False)
 # is either False to denote no periodicity
@@ -91,6 +89,12 @@ periodic           = data["periodic"          ]
 # value that determines how large the output image should be
 # output size is roughly: lf*32
 lf                 = data["lf"                ]
+
+# Training image size
+# must be a power of 2
+img_size           = data["img_size"          ]
+# z vector depth
+z_channels         = data["z_channels"        ]
 
 print("Starting SliceGAN")
 print(f"Using {data_type} as transfer type")
@@ -102,9 +106,10 @@ Project_path = util.mkdr(Project_name, Project_dir, Training)
 if Dream3d_Support:
     import os
     from dream3d_support import preprocessor, postprocessor
-    
-    ################## TO DO ####################
-    # Add ability to import text from preprocessor
+
+    ###set up preprocessor behavior
+    Normalize = True
+    scale_factor = 1
 
     ###calculate required number of image channels from orientations_types
     properties = utils_json.pull_input(os.path.realpath("dream3d_support"+"/"+"properties.json"))
@@ -119,8 +124,6 @@ if Dream3d_Support:
         image_type = 'grayscale'
         if not data_type == "text":
             data_type = 'grayscale'
-    
-    Normalize = True
 
     ###setup paths
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -147,7 +150,7 @@ if Dream3d_Support and Training:
 # Train
 if Training:
     print('Loading Dataset...')
-    datasets = preprocessing.batch(data_path, data_type, img_size, scale_factor, Normalize)
+    # datasets = preprocessing.batch(data_path, data_type, img_size, scale_factor, Normalize)
     # model.train(path_input, Project_path, image_type, datasets, netD, netG, img_channels, img_size, z_channels, n_dims)
 
 # Generate
